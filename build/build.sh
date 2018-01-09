5c4b8c8b4ac9c49a46bc04e52b224848893d3ac9
Brian Devins <brian.devins@coxautoinc.com>
2018-01-08 23:36:01 -0500

    Create a dockerfile for nvm, need to have travix build it for me now

--- build/build.sh
+++ build/build.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
-
 export COMPLETION=${TRAVIS_BUILD_DIR}/build/dotfiles-completion
 
 git clone https://${GH_TOKEN}@github.com/devinsba/dotfiles-completion $COMPLETION
 
-touch $COMPLETION/test
-
-(cd $COMPLETION && git status)
-
-if [ ! -z "$(git status --porcelain)" ]; then
+if [ ! -z "$(cd $COMPLETION && git status --porcelain)" ]; then
     cd $COMPLETION
-    setup_git
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
     git add .
     git commit -am "New completions files"
     git push --quiet --set-upstream origin $TRAVIS_BRANCH
---
+++ dockerfiles/nvm/wrapper.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+bash -l -c "nvm $@"
