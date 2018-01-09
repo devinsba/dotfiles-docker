a2c310de9e3567a06bcff41d1e9e6fbb1cdf860c
Brian Devins <brian.devins@coxautoinc.com>
2018-01-08 19:57:25 -0500

    Add new completions to the completions repo

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
-env
-git clone https://github.com/devinsba/dotfiles-completion ./build/dotfiles-completion
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+COMPLETION=${TRAVIS_BUILD_DIR}/build/dotfiles-completion
+
+git clone https://${GH_TOKEN}@github.com/devinsba/dotfiles-completion $COMPLETION
+
+if [ -z "$(git status --porcelain)" ]; then
+    cd $COMPLETION
+    setup_git
+    git add .
+    git commit -am "New completions files"
+    git push --quiet --set-upstream origin $TRAVIS_BRANCH
+fi
